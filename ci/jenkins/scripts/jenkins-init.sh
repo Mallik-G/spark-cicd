523f823789254dfcfe6c7aa0040376a3e69a270f
rnrbscigility <rb@scigility.com>
2017-06-20 14:12:02 +0200

    Added Jenkins AWS creation with Terraform

---
+++ ci/jenkins/scripts/jenkins-init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# volume setup
+sudo vgchange -ay
+
+DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
+if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
+	pvcreate ${DEVICE}
+	vgcreate data ${DEVICE}
+	lvcreate --name volume1 -l 100%FREE data
+	mkfs.ext4 /dev/data/volume1
+fi
+sudo mkdir -p /var/lib/jenkins
+echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' | sudo tee --append /etc/fstab > /dev/null
+sudo mount /var/lib/jenkins
+
+# install jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo "deb http://pkg.jenkins.io/debian-stable binary/" | sudo tee --append /etc/apt/sources.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y jenkins=${JENKINS_VERSION} unzip
+
+# install pip
+sudo wget -q https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo python3 get-pip.py
+sudo rm -f get-pip.py
+# install awscli
+sudo pip install awscli
+
+# install terraform
+cd /usr/local/bin
+wget -q https://releases.hashicorp.com/terraform/0.9.8/terraform_0.9.8_linux_amd64.zip
+unzip terraform_0.9.8_linux_amd64.zip
+# install packer
+wget -q https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_linux_amd64.zip
+unzip packer_1.0.0_linux_amd64.zip
+# clean up
+sudo apt-get clean
+rm terraform_0.9.8_linux_amd64.zip
+rm packer_1.0.0_linux_amd64.zip
---
+++ ci/jenkins/vars.tf
@@ -0,0 +1,27 @@
+variable "AWS_REGION" {
+  default = "eu-west-2"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    eu-west-1 = "ami-799b841f"
+    eu-west-2 = "ami-9e5640fa"
+    eu-entral-1 = "ami-fc5bff93"
+  }
+}
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
+variable "JENKINS_VERSION" {
+  default = " 2.46.3"
+}
+
+variable "APP_INSTANCE_COUNT" {
+  default = "0"
+}
